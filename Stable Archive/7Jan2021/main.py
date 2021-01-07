3122e72f39dc906793901137ea3e90fa180e35f5
Sean <74118019+SeanPaulCampbell@users.noreply.github.com>
2021-01-07 16:05:04 -0600

    Fully armed and operational code

---
+++ Stable Archive/7Jan2021/main.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+import Classes_Gillespie as Classy
+import PostProcessing_Functions as Post
+import Functions_Gillespie as Gill
+import numpy as np
+import pandas as pd
+from os import listdir
+from os.path import isfile, join
+from pathlib import Path
+import multiprocessing as mp
+safeProcessors = max(1, int(mp.cpu_count() * .5))
+
+
+def run_pipeline(gillespie_parameters, run_vector, path):
+    reaction_list = Initialize_Classes(gillespie_parameters[0:7])
+    initial_state = gillespie_parameters[7]
+    initial_state = np.array([initial_state], dtype=int)
+    [stop_time, burn_in_time, sample_rate] = run_vector  # remove runcount
+
+    signal = Gill.gillespie(reaction_list, stop_time, initial_state)
+
+    signalcsv = path + '{}signal.csv'.format(gillespie_parameters)
+    np.savetxt(signalcsv, gillespie_parameters[:7], delimiter=",")  # maybe make this a header
+    np.savetxt(signalcsv, signal, delimiter=",")
+
+    burned_in_signal = Post.burn_in_time_series(signal, burn_in_time)  # this needs to be passed by reference.
+    uniform_sampling = Post.uniformly_sample(burned_in_signal, sample_rate)  # this needs to be passed by reference.
+
+    peakscsv = path + "{}peaks.csv".format(gillespie_parameters)
+
+    peaks = Post.chop_peaks(uniform_sampling, peakscsv)
+    return peaks
+
+
+def Initialize_Classes(gillespie_parameters):
+    [alpha, beta, yr, r0, c0, mu, cv] = gillespie_parameters
+
+    dilution = Classy.Reaction(np.array([-1], dtype=int), 0, 0, [0, beta, 1, 0], 1, [0])
+
+    enzymatic_degradation = Classy.Reaction(np.array([-1], dtype=int), 0, 0, [0, yr, r0, 1], 1, [0])
+
+    production = Classy.Reaction(np.array([1], dtype=int), 0, 1, [alpha, c0, 2], 0, [mu, mu * cv])
+
+    reaction_list = np.array([production, enzymatic_degradation, dilution])
+    return reaction_list
+
+
+with mp.Pool(safeProcessors) as pool2:
+    alpha = [300]
+    beta = [.1]
+    gamma_r = [80]
+    R0 = [1]
+    C0 = [10]
+    mu = list(np.linspace(5, 10, 16))
+    cv = list(np.linspace(0, .5, 16))
+
+    stop_time = 500
+    burn_time = 50
+    sample_rate = 10
+
+    path_to_raw_data = "7Jan2021/"
+    Path(path_to_raw_data).mkdir(parents=True, exist_ok=True)
+
+    parameter_sets = Gill.list_for_parallelization([alpha, beta, gamma_r, R0, C0, mu, cv, [[0]]])
+    try:
+        pool2.starmap(run_pipeline, [(parameter_set, [stop_time, burn_time, sample_rate], path_to_raw_data)
+                                     for parameter_set in parameter_sets])
+    finally:
+        pool2.close()
+        pool2.join()
+
+period_mean = np.zeros([len(mu), len(cv)])
+period_cv = np.zeros([len(mu), len(cv)])
+amplitude_mean = np.zeros([len(mu), len(cv)])
+amplitude_cv = np.zeros([len(mu), len(cv)])
+
+onlyPeakfiles = [f for f in listdir(path_to_raw_data) if isfile(join(path_to_raw_data, f))]
+onlyPeakfiles = [f for f in onlyPeakfiles if "peaks.csv" in f]
+for peakfile in onlyPeakfiles:
+    peaks = np.array(pd.read_csv(path_to_raw_data + peakfile, header=None))
+    params = np.array(peakfile[1:-10].split(', ')[-3:-1], dtype=float)
+    indices = (np.where(mu == params[0])[0][0], np.where(cv == params[1])[0][0])
+
+    period_mean[indices] = np.mean(peaks[:, 0])
+    period_cv[indices] = np.std(peaks[:, 0]) / period_mean[indices]
+    amplitude_mean[indices] = np.mean(peaks[:, 2])
+    amplitude_cv[indices] = np.std(peaks[:, 2]) / amplitude_mean[indices]
+
+np.savetxt(path_to_raw_data + "period_mean.csv", period_mean, delimiter=",")
+np.savetxt(path_to_raw_data + "period_cv.csv", period_cv, delimiter=",")
+np.savetxt(path_to_raw_data + "amplitude_mean.csv", amplitude_mean, delimiter=",")
+np.savetxt(path_to_raw_data + "amplitude_cv.csv", amplitude_cv, delimiter=",")
