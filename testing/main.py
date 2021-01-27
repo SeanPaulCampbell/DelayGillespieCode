b1fe268f2aa1942413b11cf71832483b4c56d1b7
Sean <74118019+SeanPaulCampbell@users.noreply.github.com>
2021-01-26 19:21:52 -0600

    minor cleanup

--- testing/main.py
+++ testing/main.py
@@ -10,12 +10,10 @@ from pathlib import Path
 
 # Function Definitions
 def run_pipeline(gillespie_parameters, run_vector, path):
-    reaction_list = Initialize_Classes(gillespie_parameters[0:7])
-    initial_state = gillespie_parameters[7]
-
-
-    [stop_time, burn_in_time, sample_rate] = run_vector  # remove runcount
-
+    reaction_list = Initialize_Classes(gillespie_parameters[:-1])
+    initial_state = gillespie_parameters[-1]
+    [stop_time, burn_in_time, sample_rate] = run_vector 
+    
     signal = Gill.gillespie(reaction_list, stop_time, initial_state)
     signalcsv = path + '{}signal.csv'.format(gillespie_parameters)
     np.savetxt(signalcsv, gillespie_parameters[:7], delimiter=",")  # maybe make this a header
@@ -26,53 +24,29 @@ def run_pipeline(gillespie_parameters, run_vector, path):
     peakscsv = path + "{}peaks.csv".format(gillespie_parameters)
     peaks = Post.chop_peaks(uniform_sampling, peakscsv)
 
-
     return peaks
-
-
-def Initialize_ClassesRepresilator(gillespie_parameters):
-    [alpha, beta, yr, r0, c0, mu, cv] = gillespie_parameters
-    destruction_delay = [0]
-    desttoy_protein0 = [-1,0,0]
-    mobius_propensity = 0
-    reactionHill = 1
-    #    dilution0 = Classy.Reaction(np.array(desttoy_protein0, dtype=int), protein in equation, mobius_propensity, [0, beta, 1, 0], distribution_id, destruction_delay)
-    dilution0 = Classy.Reaction(np.array(desttoy_protein0, dtype=int), 0, mobius_propensity, [0, beta, 1, 0], 1, destruction_delay)
-    dilution1 = Classy.Reaction(np.array([0,-1,0], dtype=int), 1, mobius_propensity, [0, beta, 1, 0], 1, destruction_delay)
-    dilution2 = Classy.Reaction(np.array([0,0,-1], dtype=int), 2, mobius_propensity, [0, beta, 1, 0], 1, destruction_delay)
-
-    enzymatic_degradation0 = Classy.Reaction(np.array([-1,0,0], dtype=int), 0, mobius_propensity, [0, yr, r0, 1], 1, destruction_delay) # need to rewrite this so that all molecules are in the base
-    enzymatic_degradation1 = Classy.Reaction(np.array([0,-1,0], dtype=int), 1, mobius_propensity, [0, yr, r0, 1], 1, destruction_delay) #  need to make it such that  denominator is R 0 +r 1 (t)+r 2 (t)+r 3 (t)
-    enzymatic_degradation2 = Classy.Reaction(np.array([0,0,-1], dtype=int), 2, mobius_propensity, [0, yr, r0, 1], 1, destruction_delay)
-
-    production1 = Classy.Reaction(np.array([0,1,0], dtype=int), 0, reactionHill, [alpha, c0, 2], 0, [mu, mu * cv])
-    production0 = Classy.Reaction(np.array([1,0,0], dtype=int), 2, reactionHill, [alpha, c0, 2], 0, [mu, mu * cv])
-
-    production2 = Classy.Reaction(np.array([0,0,1], dtype=int), 1, reactionHill, [alpha, c0, 2], 0, [mu, mu * cv])
-
-    reaction_list = np.array([production1, production0, production2, enzymatic_degradation0, enzymatic_degradation1, enzymatic_degradation2, dilution0,dilution1,dilution2])
-    return reaction_list
-
 
 
 def Initialize_Classes(gillespie_parameters):
     [alpha, beta, yr, r0, c0, mu, cv] = gillespie_parameters
+    
+    dilution0 = Classy.Reaction(np.array([-1, 0, 0], dtype=int), 0, 'mobius_propensity', [0, beta, 1, 0], 1, [0])
+    dilution1 = Classy.Reaction(np.array([0, -1, 0], dtype=int), 1, 'mobius_propensity', [0, beta, 1, 0], 1, [0])
+    dilution2 = Classy.Reaction(np.array([0, 0, -1], dtype=int), 2, 'mobius_propensity', [0, beta, 1, 0], 1, [0])
 
-    dilution = Classy.Reaction(np.array([-1], dtype=int), 0, 0, [0, beta, 1, 0], 1, [0])
+    degradation0 = Classy.Reaction(np.array([-1, 0, 0], dtype=int), 0, 'mobius_propensity', [0, yr, r0, 1], 1, [0]) # need to rewrite this so that all molecules are in the base
+    degradation1 = Classy.Reaction(np.array([0, -1, 0], dtype=int), 1, 'mobius_propensity', [0, yr, r0, 1], 1, [0]) #  need to make it such that  denominator is R 0 +r 1 (t)+r 2 (t)+r 3 (t)
+    degradation2 = Classy.Reaction(np.array([0, 0, -1], dtype=int), 2, 'mobius_propensity', [0, yr, r0, 1], 1, [0])
 
-    enzymatic_degradation = Classy.Reaction(np.array([-1], dtype=int), 0, 0, [0, yr, r0, 1], 1, [0])
+    production1 = Classy.Reaction(np.array([0,1,0], dtype=int), 0, 'decreasing_hill_propensity', [alpha, c0, 2], 0, [mu, mu * cv])
+    production0 = Classy.Reaction(np.array([1,0,0], dtype=int), 2, 'decreasing_hill_propensity', [alpha, c0, 2], 0, [mu, mu * cv])
+    production2 = Classy.Reaction(np.array([0,0,1], dtype=int), 1, 'decreasing_hill_propensity', [alpha, c0, 2], 0, [mu, mu * cv])
 
-    production = Classy.Reaction(np.array([1], dtype=int), 0, 1, [alpha, c0, 2], 0, [mu, mu * cv])
-
-    reaction_list = np.array([production, enzymatic_degradation, dilution])
+    reaction_list = np.array([production1, production0, production2, degradation0, degradation1, degradation2, dilution0,dilution1,dilution2])
     return reaction_list
-# Get parallel list running
-# RUN FOR LOOP MATH MACHINE
-
-# MAKE FILE THAT  makes code into compiled form compile.py
 
 
-#Note: keep function defitions above mp.Pool the 2 shall never mix. Function definitions are like an oil spill on top of water
+#Note: keep function defitions above mp.Pool
 with mp.Pool(safeProcessors) as pool2:
     alpha = [300]
     beta = [.1]
@@ -80,7 +54,7 @@ with mp.Pool(safeProcessors) as pool2:
     R0 = [1]
     C0 = [10]
     mu = [5]
-    cv = [.1]
+    cv = [0]
 
     stop_time = 500
     burn_time = 50
@@ -88,16 +62,12 @@ with mp.Pool(safeProcessors) as pool2:
     path = "Jan/"
     Path(path).mkdir(parents=True, exist_ok=True)
 
-    #parameter_sets = Gill.list_for_parallelization([alpha, beta, gamma_r, R0, C0, mu, cv, [[0]]])
     initial_state_represilator = np.array([0,50,200], dtype=int)
-    #[np.array([0,50,200])]
-    #np.array([0,50,200], dtype=int)
-    parameter_sets_represilator = Gill.list_for_parallelization([alpha,[.05,.2],gamma_r, R0,C0, [5,10],[.05,.5],[initial_state]])
-    parameter_sets = Gill.list_for_parallelization([alpha, [.05, .2], gamma_r, R0, C0, [5, 10], [.05, .5], [[0]]])
-
+    parameter_sets_represilator = Gill.list_for_parallelization([alpha, beta, gamma_r, R0, C0, mu, cv, [initial_state]])
+    
     try:
-        pool2.starmap(run_pipeline,
-                          [(parameter_set, [stop_time, burn_time, sample_rate], path) for parameter_set in parameter_sets_represilator])
+        pool2.starmap(run_pipeline, [(parameter_set, [stop_time, burn_time, sample_rate], path) 
+                                     for parameter_set in parameter_sets_represilator])
     finally:
         pool2.close()
         pool2.join()
