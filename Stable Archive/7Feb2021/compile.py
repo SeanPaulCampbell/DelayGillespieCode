d3b1b7454ec7a09a60ecc584620716e83004edbc
Sean <74118019+SeanPaulCampbell@users.noreply.github.com>
2021-02-07 20:38:16 -0600

    Add files via upload

---
+++ Stable Archive/7Feb2021/compile.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import os
+import py_compile
+PYTHONOPTIMIZE = "OO"
+
+
+py_compile.compile("Classes_Gillespie.py", "__pycache__/Classes_Gillespie.pyc", optimize=2)
+py_compile.compile("Functions_Gillespie.py", "__pycache__/Functions_Gillespie.pyc", optimize=2)
+py_compile.compile("main.py", "__pycache__/main.pyc", optimize=2)
+py_compile.compile("PostProcessing_Functions.py", "__pycache__/PostProcessing_Functions.pyc", optimize=2)
+os.system('python __pycache__/main.pyc')
+exit()
